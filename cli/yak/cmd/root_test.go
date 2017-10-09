2238241ad7bcf96c5024a7a5096812d84ab97e13
Shawn Wang <shawn.wang@easystack.cn>
2017-10-09 14:44:41 +0800

    fix fmt

--- cli/yak/cmd/root_test.go
+++ cli/yak/cmd/root_test.go
@@ -10,20 +10,20 @@ import (
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { TestingT(t) }
 
-type YakSuite struct {}
+type YakSuite struct{}
 
 var _ = Suite(&YakSuite{})
 
 func mockArgs(args ...string) (restore func()) {
-        old := os.Args
-        os.Args = args
-        return func() { os.Args = old }
+	old := os.Args
+	os.Args = args
+	return func() { os.Args = old }
 }
 
 func (s *YakSuite) TestUnknownCommentErrorResult(c *C) {
 	restore := mockArgs("yak", "foo")
 	defer restore()
 
-        err := RootCmd.Execute()
-        c.Assert(err, ErrorMatches, `unknown command \"foo\" for \"yak\"`)
+	err := RootCmd.Execute()
+	c.Assert(err, ErrorMatches, `unknown command \"foo\" for \"yak\"`)
 }

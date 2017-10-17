c814becc863c99bfadda151dfbd8692894feed5d
Shawn Wang <shawn.wang@easystack.cn>
2017-10-17 10:34:22 +0800

    add appCmd for info and install commands

--- cli/yak/cmd/info.go
+++ cli/yak/cmd/info.go
@@ -16,30 +16,14 @@
 package cmd
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/spf13/cobra"
 	"gitlab.com/EasyStack/yakety/lib/recipe"
 )
 
 // infoCmd represents the info command
-var infoCmd = &cobra.Command{
-	Use:   "info [app]",
-	Short: "list all installed recipes",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var infoCmd = appCmd("info")
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("info Print: " + strings.Join(args, " "))
-		fmt.Println("app: " + args[0])
-		r := recipe.LoadRecipe(args[0])
-		r.Dump()
-	},
+func info(r recipe.IRecipeConfig) {
+	r.Dump()
 }
 
 func init() {

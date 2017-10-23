90fa086ea65fa0203571a278814affd2e1c46c54
Shawn Wang <shawn.wang@easystack.cn>
2017-10-23 09:37:00 +0800

    add fetch command to fetch yak recipe repo

---
+++ cli/yak/cmd/fetch.go
@@ -0,0 +1,49 @@
+// Copyright © 2017 Shawn Wang <shawn.wang@easystack.cn>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// fetchCmd represents the fetch command
+var fetchCmd = &cobra.Command{
+	Use:   "fetch [repo]",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fetch(args[0], YakRoot())
+	},
+}
+
+func fetch(repo string, root string) {
+	cmd := exec.Command("git", "clone", "https://"+repo, root+"/"+repo)
+	cmd.Run()
+	fmt.Printf("git clone https://%s", repo)
+}
+
+func init() {
+	RootCmd.AddCommand(fetchCmd)
+}

9fe0b55a44624c01848a551a4a56278cc1150dc6
Shawn Wang <shawn.wang@easystack.cn>
2017-11-13 14:01:21 +0800

    save local diff as abc.tar.gz
    Todo: change the tar name as ostree commit checksum

--- lib/recipe/atomic.go
+++ lib/recipe/atomic.go
@@ -1,7 +1,12 @@
 package recipe
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -10,11 +15,99 @@ type AtomicRecipeConfig struct {
 	BaseRecipeConfig
 }
 
+func addFile(tw *tar.Writer, path string) error {
+	fmt.Printf("prepare: %s.\n", path)
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if stat, err := file.Stat(); err == nil {
+		// now lets create the header as needed for this file within the tarball
+		header := new(tar.Header)
+		header.Name = path
+		if !stat.IsDir() {
+			header.Size = stat.Size()
+			header.ModTime = stat.ModTime()
+			header.Mode = int64(stat.Mode())
+		}
+
+		if err := tw.WriteHeader(header); err != nil {
+			fmt.Printf(" >> n: %s \n", header.Name)
+			fmt.Printf(" >> size: %d \n", header.Size)
+			fmt.Printf(" >> m: %d \n", header.Mode)
+			fmt.Printf(" >> err: %s \n", err)
+			return err
+		}
+
+		if !stat.IsDir() {
+			// write the header to the tarball archive
+			// copy the file data to the tarball
+			fmt.Printf("copying %s ... m: %d\n", path, header.Mode)
+			if _, err := io.Copy(tw, file); err != nil {
+				return err
+			}
+			fmt.Printf("copied %s.\n", path)
+		}
+	}
+	return nil
+}
+
 func (r AtomicRecipeConfig) IsInstallable() bool {
 	return false
 }
 
+func createTarGz(name string, files []string) bool {
+	// set up the output file
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	// set up the gzip writer
+	gw := gzip.NewWriter(file)
+	defer gw.Close()
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+	// grab the paths that need to be added in
+
+	for _, i := range files {
+		if err := addFile(tw, i); err != nil {
+			log.Fatalln(err)
+			return false
+		}
+	}
+	return true
+}
+
+func configDiff() []string {
+	out, err := exec.Command("ostree", "admin", "config-diff").Output()
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+	// fmt.Printf("The date is %s\n", out)
+	data := fmt.Sprintf("%s", out)
+	result := strings.Split(strings.TrimSpace(data), "\n")
+
+	for index, each := range result {
+		file := each[5:len(each)]
+		result[index] = "/etc/" + file
+	}
+	return result
+}
+
 func (r AtomicRecipeConfig) Install() bool {
+	// backup current local config
+	// ostree admin config-diff
+
+	files := configDiff()
+	result := createTarGz("abc.tar.gz", files)
+	if !result {
+		return false
+	}
+
 	remoteName := strings.Split(r.Branch, "/")[0]
 	fmt.Printf(">>> install : n:%s s:%s c:%s \n", r.Name, r.Source, r.Commit)
 	// ostree remote add --if-not-exists --no-gpg-verify fedora-atomic-26 https://kojipkgs.fedoraproject.org/atomic/26

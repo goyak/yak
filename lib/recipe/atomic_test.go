dac8908a8e3fabc9170a9930ffe6b14d39228ed2
Shawn Wang <shawn.wang@easystack.cn>
2017-11-20 22:23:57 +0800

    rename test output var name: fakeRunResult

--- lib/recipe/atomic_test.go
+++ lib/recipe/atomic_test.go
@@ -20,11 +20,11 @@ func TestHelperProcess(t *testing.T) {
 		return
 	}
 	// some code here to check arguments perhaps?
-	fmt.Fprintf(os.Stdout, dockerRunResult)
+	fmt.Fprintf(os.Stdout, fakeRunResult)
 	os.Exit(0)
 }
 
-const dockerRunResult = `{
+const fakeRunResult = `{
     "deployments": [
         {
             "base-checksum": "aa467cfa26d8c9914275c692219ec018c1f31dfc036b1bf4fa1d3ab227a2a8d0",
